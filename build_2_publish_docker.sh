ab6c90c610595c38069b8a554c2b6ebbe678ae78
Brandon Miller <millertimebjm@hotmail.com>
2023-09-23 22:16:41 -0500

    add environment variables to deployment artifacts

--- build_2_publish_docker.sh
+++ build_2_publish_docker.sh
@@ -2,11 +2,16 @@
 
 IMAGE_NAME="oahousechatgpt"
 
+args=""
+for ((i=0; i<${#keys[@]}; i++)); do
+  args+=" -e ${keys[$i]}=${values[$i]}"
+done
+
 dotnet build
 
 
 dotnet publish --os linux --arch x64 /t:PublishContainer -c Release
-docker run -d -p 7080:7080 --restart=always --name $IMAGE_NAME $IMAGE_NAME:1.0.0
+docker run -d -p 7080:7080 --restart=always --name $IMAGE_NAME $IMAGE_NAME:1.0.0 $args
 rm -rf /tmp/Containers
 
 
